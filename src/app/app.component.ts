f852a6021774e0af5850c4df7baf2eb7ad20fae2
Daniel Koeman <Daniel Koeman>
2017-04-14 20:48:07 +0200

    paar bestandjes toegevoegd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Market } from './viewmodels/market';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{welcomeText}}</h1>`,
+  template: `<h1>{{title}}</h1><h2>{{market.name}} details!</h2>`
 })
-export class AppComponent  
-{ 
-  welcomeText = 'Welkom'; 
+
+export class AppComponent {
+  title = 'Marktkaart';
+  market: Market = {
+    id: 1,
+    name: 'Test Markt'
+  };
 }
---
+++ src/app/viewmodels/market.ts
@@ -0,0 +1,4 @@
+export class Market {
+    id: number;
+    name: string;
+}
