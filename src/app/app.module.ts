136bdd78eaa3fd5aaef22f0d608612cd4124ff26
danilovice <dckoeman@gmail.com>
2017-12-14 13:23:37 +0100

    added messages component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppComponent } from './app.component';
 import { MarktenComponent } from './markten/markten.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarktenComponent,
-    HomeComponent
+    HomeComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
